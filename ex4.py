a2289a567aa3d1904e5cb60a888939937c84ed1a
sei845 <94697098+sei845@users.noreply.github.com>
2021-11-22 13:28:44 +0300

    Add files via upload

--- ex4.py
+++ ex4.py
@@ -1,11 +1,9 @@
 n = int(input("Введите целое положительное число: "))
 max = n % 10
-while n >= 1:
+while n > 0:
     n = n // 10
     if n % 10 > max:
         max = n % 10
-    if n > 9:
-        continue
     else:
         print("Максимальная цифра в числе: ", max)
         break
