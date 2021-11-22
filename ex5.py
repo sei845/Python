dd8cfb7c92e7b1f63997f017505813243abb64da
sei845 <94697098+sei845@users.noreply.github.com>
2021-11-22 13:19:50 +0300

    Add files via upload

--- ex5.py
+++ ex5.py
@@ -1,7 +1,7 @@
 profit = float(input("Введите выручку фирмы: "))
 izd = float(input("Введите издержки фирмы: "))
 if profit > izd:
-    print("Фирма работает с прибылью. Рентабельность: %.2f" % (profit / izd))
+    print(f"Фирма работает с прибылью. Рентабельность:  {((profit-izd) / profit)*100:.2f} %")
     workers = int(input("Введите количество сотрудников: "))
     print("Прибыль на каждого сторудника сотавила: %.2f" % (profit / workers))
 elif profit == izd:
